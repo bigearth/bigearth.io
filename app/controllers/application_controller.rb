77f1b6d393ea9c42b96434b979e69bf0a0abe3f5
Carlos Cardona <cgcardona@gmail.com>
2016-04-16 18:26:52 -0700

    feature/landing_page: clean up application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :get_coin_value
-    
-  def get_coin_value
-    blockr = BigEarth::Blockchain::Blockr.new
-    coin_info = blockr.coin
-    @exchange_rate = coin_info['data']['markets']['coinbase']['value']
-  end
-  
-  def after_sign_in_path_for user
-    user_path user
-  end
 end
