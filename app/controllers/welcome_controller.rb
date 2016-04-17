0ba141151b0b9b3aee44ae79e7f6f8a7dee9aea4
Carlos Cardona <cgcardona@gmail.com>
2016-04-16 19:21:45 -0700

    feature/ssl: add let's encrypt for staging.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,7 @@ class WelcomeController < ApplicationController
   # GET /welcome
   def welcome
   end
+  def letsencrypt
+    render text: Figaro.env.lets_encrypt_key
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#welcome'
+  get '/.well-known/acme-challenge/:id' => 'welcome#letsencrypt'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
